5b9f28fa4a67ff06134a0395ede1334174058394
Pouya Rostam <pouya@picovoice.ai>
2023-05-15 14:22:28 -0700

    fixed actions

--- demo/python/eagle_demo_mic.py
+++ demo/python/eagle_demo_mic.py
@@ -125,7 +125,7 @@ def main():
                 access_key=args.access_key,
                 model_path=args.model_path,
                 library_path=args.library_path)
-        except pveagle.EagleInvalidArgumentError as e:
+        except pveagle.EagleInvalidArgumentError:
             print("One or more arguments provided to Eagle is invalid: ", args)
             print("If all other arguments seem valid, ensure that '%s' is a valid AccessKey" % args.access_key)
             raise
